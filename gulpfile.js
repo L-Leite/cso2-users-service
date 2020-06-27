46464626e165f0828a462d7f398e61d7126db14b
Luís Leite <luis@leite.xyz>
2020-06-28 00:55:19 +0100

    Use eslint instead of tslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,51 @@
-'use strict'
-
-var cached = require('gulp-cached')
-var del = require('delete')
-var gulp = require('gulp')
-var mocha = require('gulp-mocha')
-var sourcemaps = require('gulp-sourcemaps')
-var ts = require('gulp-typescript')
-var tslint = require('gulp-tslint')
-var typedoc = require('gulp-typedoc')
-var util = require('gulp-util')
-
-gulp.task('clean:dist', () => {
-  util.log('Cleaning transpiled source...')
-  return del(['dist/**/*'])
-})
-
-gulp.task('clean:docs', () => {
-  util.log('Cleaning documentation...')
-  return del(['docs/**/*'])
-})
+const eslint = require('gulp-eslint')
+const gulp = require('gulp')
+const log = require('fancy-log')
+const mocha = require('gulp-mocha')
+const ts = require('gulp-typescript')
+const typedoc = require('gulp-typedoc')
 
 gulp.task('readme', () => {
-  util.log('Generating documentation...')
-  return gulp.src(['src/**/*.ts'])
-    .pipe(typedoc({
-      excludeExternals: true,
-      ignoreCompilerErrors: false,
-      includeDeclarations: true,
-      name: 'cso2-users-service',
-      out: './docs',
-      plugins: ['mdFlavour github'],
-      theme: 'markdown',
-      tsconfig: 'tsconfig.json',
-      version: true
-    }))
+    log('Generating documentation...')
+    return gulp.src(['src/**/*.ts']).pipe(
+        typedoc({
+            excludeExternals: true,
+            ignoreCompilerErrors: false,
+            includeDeclarations: true,
+            name: 'cso2-users-service',
+            out: './docs',
+            plugins: ['mdFlavour github'],
+            theme: 'markdown',
+            tsconfig: 'tsconfig.json',
+            version: true
+        })
+    )
 })
 
-gulp.task('tslint', () => {
-  util.log('Linting source code...')
-  const cfg = ts.createProject('tsconfig.json')
-  return cfg.src()
-    .pipe(cached('tslint'))
-    .pipe(tslint({
-      formatter: 'verbose'
-    }))
-    .pipe(tslint.report({
-      summarizeFailureOutput: true
-    }))
+gulp.task('eslint', () => {
+    log('Linting source code...')
+    return gulp
+        .src(['src/**/*.ts'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
 })
 
 gulp.task('typescript', () => {
-  util.log('Transpiling source code...')
-  const project = ts.createProject('tsconfig.json')
-  return project.src()
-    .pipe(cached('typescript'))
-    .pipe(sourcemaps.init())
-    .pipe(project())
-    .pipe(sourcemaps.write('.', {
-      includeContent: false,
-      sourceRoot: '../src'
-    }))
-    .pipe(gulp.dest('dist'))
+    log('Transpiling source code...')
+    const project = ts.createProject('tsconfig.json')
+    return project.src().pipe(project()).pipe(gulp.dest('dist'))
 })
 
-gulp.task('build', gulp.series(
-  'clean:dist',
-  'tslint',
-  'typescript'
-))
-
-gulp.task('typedoc', gulp.series(
-  'clean:docs',
-  'readme'
-))
+gulp.task('build', gulp.series('eslint', 'typescript'))
 
 gulp.task('test', () => {
-  util.log('Testing code...')
-  return gulp.src('test/**/*.ts')
-    .pipe(mocha({
-      require: 'ts-node/register'
-    }))
+    log('Testing code...')
+    return gulp.src('test/**/*.ts').pipe(
+        mocha({
+            require: 'ts-node/register'
+        })
+    )
 })
 
-gulp.task('default', gulp.series(
-  'clean:dist',
-  'typescript'
-))
+gulp.task('default', gulp.series('typescript'))
