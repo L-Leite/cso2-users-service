c0adb4344ade33f61be271bc62a730f9b819f021
Luís Leite <luis@leite.xyz>
2020-07-20 17:21:37 +0100

    CI: fix paths in build script

--- ci/build.sh
+++ ci/build.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
 # init db
-createdb -O cso2
-psql cso2 < $SCRIPTPATH/../config/db/users.sql
-psql cso2 < $SCRIPTPATH/../config/db/inventories.sql
-psql cso2 < $SCRIPTPATH/../config/db/inventory/buymenues.sql
-psql cso2 < $SCRIPTPATH/../config/db/inventory/cosmetics.sql
-psql cso2 < $SCRIPTPATH/../config/db/inventory/loadouts.sql
+createdb cso2
+psql cso2 < ./config/db/users.sql
+psql cso2 < ./config/db/inventories.sql
+psql cso2 < ./config/db/inventory/buymenues.sql
+psql cso2 < ./config/db/inventory/cosmetics.sql
+psql cso2 < ./config/db/inventory/loadouts.sql
 new_oid=$(psql cso2 -c "SELECT oid FROM pg_type WHERE typname = 'inventoryitem';" | sed -n 3p | tr -d ' ')
-echo "export const INVENTORY_ITEM_OID = $new_oid" > $SCRIPTPATH/../src/config/inventory_item_oid.ts
+echo "export const INVENTORY_ITEM_OID = $new_oid" > ./src/config/inventory_item_oid.ts
 
 # use the CI database user
-sed -i -e "s/username: 'cso2_user'/username: 'postgres'/g" $SCRIPTPATH/../src/config/db.ts
-sed -i -e "s/password: 'cso2'/\/\/ password: 'cso2'/g" $SCRIPTPATH/../src/config/db.ts
+sed -i -e "s/username: 'cso2_user'/username: 'postgres'/g" ./src/config/db.ts
+sed -i -e "s/password: 'cso2'/\/\/ password: 'cso2'/g" ./src/config/db.ts
 
 # transpile source code
 npx gulp build
