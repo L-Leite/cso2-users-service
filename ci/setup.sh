91ab65dd671ddaf44f121e2d30c799efdf39cfe2
Luís Leite <luis@leite.xyz>
2020-07-20 13:55:22 +0100

    CI: try to fix init DB script again

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-# init db
-./config/db/init_db.sh
-
 # install and use the current node version set in the environment variable
 nvm install $CURRENT_NODE_VERSION
 nvm use $CURRENT_NODE_VERSION
-
-# update npm to latest version
-npm -g i npm
 
 # print tools versions
 echo 'NodeJS version:'
